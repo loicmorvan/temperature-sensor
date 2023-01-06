780c03f7fed748194131c5adb176408928af5b8e
Loïc Morvan <loic.morvan.fr@gmail.com>
2023-01-06 12:13:46 +0100

    Hide the HomeKit into the cpp with a pimpl.

--- src/HomeKitAccessory.h
+++ src/HomeKitAccessory.h
@@ -1,17 +1,12 @@
-#include <ESP32HomeKit.h> // TODO: Avoid including HomeKit everywhere this class is used.
-
 class HomeKitAccessory
 {
 private:
-    hap_serv_t *service;
+    class Pimpl;
+    Pimpl *pimpl;
 
 public:
     HomeKitAccessory();
 
     void SetTemperature(const float &value);
     void ResetPairings() const;
-
-private:
-    hap_serv_t *create_temperature_service();
-    hap_acc_t *create_accessory();
 };
